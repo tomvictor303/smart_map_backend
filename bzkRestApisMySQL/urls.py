f0d56833f2fe2799d18b1bd10696cb6813f36fcf
Tommy <tomvictor303@gmail.com>
2023-10-31 12:11:17 -0400

    feat: plan api GET POST

--- bzkRestApisMySQL/urls.py
+++ bzkRestApisMySQL/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   # url(r'^admin/', admin.site.urls),
   url(r'^api/tutorials/', include('tutorials.urls')),
   url(r'^api/datasets/', include('datasets.urls')),
+  url(r'^api/plans/', include('plans.urls')),
 ]
--- plans/views.py
+++ plans/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
-
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
 from rest_framework import status
@@ -24,15 +21,29 @@ def plan_list(request):
     elif request.method == 'POST':
         plan_data = JSONParser().parse(request)
         plan_serializer = PlanSerializer(data=plan_data)
-        if plan_serializer.is_valid():
-            plan_serializer.save()
-            return JsonResponse(plan_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(plan_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if not plan_serializer.is_valid():            
+          return JsonResponse(plan_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # Save only if it does not already exist
+        if not Plan.objects.filter(person=plan_data['person'], date=plan_data['date'], task=plan_data['task']).exists():
+          plan_serializer.save()
+        return JsonResponse(plan_serializer.data, status=status.HTTP_201_CREATED) 
     
     elif request.method == 'DELETE':
         to_delete_id = request.GET.get('id', None)
         if to_delete_id is not None:
-            count = Plan.objects.filter(id=to_delete_id)
+            count = Plan.objects.filter(id=to_delete_id).delete()
             return JsonResponse({'message': 'Deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
         return JsonResponse({'message': 'Item with provided ID is not exist!'}, status=status.HTTP_204_NO_CONTENT)
  
+# @api_view(['POST'])
+# def plan_delete(request):
+#     if request.method == 'POST':
+#         plan_data = JSONParser().parse(request)
+#         plan_serializer = PlanSerializer(data=plan_data)
+#         if not plan_serializer.is_valid():            
+#           return JsonResponse(plan_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         # Save only if it does not already exist
+#         if not Plan.objects.filter(person=plan_data['person'], date=plan_data['date'], task=plan_data['task']).exists():
+#           plan_serializer.save()
+#         return JsonResponse(plan_serializer.data, status=status.HTTP_201_CREATED) 
+    
