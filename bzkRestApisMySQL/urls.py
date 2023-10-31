f4bb5e34c1f05df1149989819e7c419529b0a790
Tommy <tomvictor303@gmail.com>
2023-10-30 21:29:37 -0400

    feat: tested admin site

--- bzkRestApisMySQL/urls.py
+++ bzkRestApisMySQL/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include 
+from django.contrib import admin
  
 urlpatterns = [ 
-    url(r'^', include('tutorials.urls')),
+  # url(r'^admin/', admin.site.urls),
+  url(r'^api/tutorials', include('tutorials.urls')),
 ]
